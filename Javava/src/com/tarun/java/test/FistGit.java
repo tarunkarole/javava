4885ba94be5ecca1f3ba1c19e1b0c084d586e0da
Tarun Karole <tarunk405@gmail.com>
2018-07-15 22:34:54 +0530

    my First commit

---
+++ Javava/src/com/tarun/java/test/FistGit.java
@@ -0,0 +1,10 @@
+package com.tarun.java.test;
+
+public class FistGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");// first line code
+	}
+
+}
